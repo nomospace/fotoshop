027fe25282ee5fe96a585f4f8923737c4b9ecf12
nomospace <jinlu_hz@163.com>
2013-03-06 13:34:30 +0800

    Rev for 0.0.1

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+define(['mod/curve'], function(Curve) {
+  'use strict';
+  console.log(Curve);
+});
---
+++ src/js/module/curve.js
@@ -0,0 +1,6 @@
+define(function(require, exports) {
+  'use strict';
+  exports.show = function() {
+    console.log('curve mod load!');
+  };
+});
